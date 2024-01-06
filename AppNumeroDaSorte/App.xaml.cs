e97a8aaa8263fde9b0714235c24527674c98e372
Matheus <matheuznardi@gmail.com>
2024-01-06 15:13:32 -0300

    Primeiros apps

---
+++ AppNumeroDaSorte/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppNumeroDaSorte
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //CurrentPage - Pagina Atual
+            MainPage = new MainPage();
+        }
+    }
+}
