a95240164b2bd13506c68ca40cd2b059afd1beac
Matheus <matheuznardi@gmail.com>
2024-01-11 13:51:12 -0300

    events entry

--- AppMauiGallery/Views/Components/Forms/EntryPage.xaml.cs
+++ AppMauiGallery/Views/Components/Forms/EntryPage.xaml.cs
@@ -9,4 +9,25 @@ public partial class EntryPage : ContentPage
 		
 		
 	}
+
+    
+    private void NameEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        lblTextName.Text = $"Old name {e.OldTextValue} - New name {e.NewTextValue}";
+    }
+
+    private void NameEntry_Completed(object sender, EventArgs e)
+    {
+        LblTextPassword.Text =$"Name Final: {NameEntry.Text}";
+    }
+
+    private void Entry_Completed(object sender, EventArgs e)
+    {
+        LblTextPassword.Text = $"Password: {PasswordEntry.Text}";
+    }
+
+    private void Entry_Completed_1(object sender, EventArgs e)
+    {
+       LblTextPhone.Text = $"Phone: {PhoneEntry.Text}";
+    }
 }
